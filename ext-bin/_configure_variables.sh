712d31d7525a58f0a31a0a7ba4bdb53fabe578b6
Artem Chernatsky <artem.chernatskyi@hpe.com>
2022-06-09 17:23:52 +0300

    EZNIFI-52 Added configuration for unsecure cluster

--- ext-bin/_configure_variables.sh
+++ ext-bin/_configure_variables.sh
@@ -8,5 +8,6 @@ WARDEN_CONF="$NIFI_HOME/conf/warden.nifi.conf"
 MAPR_USER=`logname`
 MAPR_GROUP="$MAPR_USER"
 BOOTSTRAP_CONF="$NIFI_HOME/conf/bootstrap.conf"
+NIFI_CONF="$NIFI_HOME/conf/nifi.properties"
 FIPS_CONF="${MAPR_HOME}/conf/java.security.fips"
 IS_SECURED=`cat ${MAPR_HOME}/conf/mapr-clusters.conf | sed 's/.*\(secure=\)\(true\|false\).*/\2/'`
