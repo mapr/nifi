085434d86a846e449a0a7d4ce12de5de054174fd
Artem Chernatsky <artem.chernatskyi@hpe.com>
2022-07-04 13:18:22 +0300

    EZNIFI-102 Added Hadoop 3 configs auto-detect

--- nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/processors/hadoop/AbstractHadoopProcessor.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/processors/hadoop/AbstractHadoopProcessor.java
@@ -50,6 +50,7 @@ import org.apache.nifi.security.krb.KerberosKeytabUser;
 import org.apache.nifi.security.krb.KerberosPasswordUser;
 import org.apache.nifi.security.krb.KerberosUser;
 import org.ietf.jgss.GSSException;
+import org.apache.nifi.util.MapRPropertiesUtils;
 
 import javax.net.SocketFactory;
 import java.io.File;
@@ -397,6 +398,12 @@ public abstract class AbstractHadoopProcessor extends AbstractProcessor implemen
     protected List<String> getConfigLocations(PropertyContext context) {
             final ResourceReferences configResources = context.getProperty(HADOOP_CONFIGURATION_RESOURCES).evaluateAttributeExpressions().asResources();
             final List<String> locations = configResources.asLocations();
+
+            // Adding auto-detected configs if no configs were provided
+            if(locations.isEmpty()) {
+                locations.addAll(MapRPropertiesUtils.getHadoopConfigs());
+            }
+
             return locations;
     }
 
