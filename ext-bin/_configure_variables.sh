ab41ca844724e0c1ace4b5ebbe8e7885e770e662
Artem Chernatsky <artem.chernatskyi@hpe.com>
2022-09-02 11:27:20 +0300

    EZNIFI-147 Added restart on config changes to configure.sh

--- ext-bin/_configure_variables.sh
+++ ext-bin/_configure_variables.sh
@@ -6,9 +6,11 @@ NIFI_HOME="$MAPR_HOME/nifi/nifi-$NIFI_VERSION"
 NIFI_LIBS=${NIFI_HOME}"/lib/"
 NIFI_NOT_USED_LIBS=${NIFI_HOME}"/not-used-libs/"
 MAPR_WARDEN_CONF_DIR="${MAPR_HOME}/conf/conf.d"
+MAPR_WARDEN_CONF="${MAPR_WARDEN_CONF_DIR}/warden.nifi.conf"
 WARDEN_CONF="$NIFI_HOME/conf/warden.nifi.conf"
-MAPR_USER=`logname`
-MAPR_GROUP="$MAPR_USER"
+DAEMON_CONF="${MAPR_HOME}/conf/daemon.conf"
+MAPR_USER=${MAPR_USER:-$( [ -f "$DAEMON_CONF" ] && awk -F = '$1 == "mapr.daemon.group" { print $2 }' "$DAEMON_CONF" )}
+MAPR_USER=${MAPR_USER:-"mapr"}
 BOOTSTRAP_CONF="$NIFI_HOME/conf/bootstrap.conf"
 NIFI_CONF="$NIFI_HOME/conf/nifi.properties"
 FIPS_CONF="${MAPR_HOME}/conf/java.security.fips"
