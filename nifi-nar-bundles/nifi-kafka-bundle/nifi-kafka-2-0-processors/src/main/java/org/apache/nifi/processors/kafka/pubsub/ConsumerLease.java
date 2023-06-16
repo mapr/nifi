a82b7336a578bdb2036612f3061cc6d2f098a4dc
exceptionfactory <exceptionfactory@apache.org>
2023-06-16 19:01:36 +0000

    NIFI-11717 Updated Required Java Version to 17.0.6
    - Added Groovy Plugin Repository for groovy-eclipse-compiler
    
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #7397.
    
    (cherry picked from commit b6b621bf9e099e8bd32365e72495a6e8e6c0c52e)

--- nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-2-0-processors/src/main/java/org/apache/nifi/processors/kafka/pubsub/ConsumerLease.java
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-2-0-processors/src/main/java/org/apache/nifi/processors/kafka/pubsub/ConsumerLease.java
@@ -535,7 +535,7 @@ public abstract class ConsumerLease implements Closeable, ConsumerRebalanceListe
                     try {
                         reader = readerFactory.createRecordReader(attributes, in, recordBytes.length, logger);
                     } catch (final IOException e) {
-                        yield();
+                        this.yield();
                         rollback(topicPartition);
                         handleParseFailure(consumerRecord, session, e, "Failed to parse message from Kafka due to comms failure. Will roll back session and try again momentarily.");
                         closeWriter(writer);
@@ -566,7 +566,7 @@ public abstract class ConsumerLease implements Closeable, ConsumerRebalanceListe
                                     logger.error("Failed to obtain Schema for FlowFile. Will roll back the Kafka message offsets.", e);
 
                                     rollback(topicPartition);
-                                    yield();
+                                    this.yield();
 
                                     throw new ProcessException(e);
                                 }
