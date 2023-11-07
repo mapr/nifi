0bd681703df834cbc423a202a527310216edb610
rymarm <rymarm@apache.org>
2023-11-07 16:29:52 +0200

    EZNIFI-228: packages has wrong version

--- devops/scripts/_initialize_package_variables.sh
+++ devops/scripts/_initialize_package_variables.sh
@@ -3,8 +3,8 @@
 GIT_COMMIT=$(git log -1 --pretty=format:"%H")
 INSTALLATION_PREFIX=${INSTALLATION_PREFIX:="/opt/mapr"}
 PKG_NAME=${PKG_NAME:="nifi"}
-PKG_VERSION=${PKG_VERSION:=1.16.3.0}
-PKG_3DIGIT_VERSION=${PKG_3DIGIT_VERSION:=1.16.3}
+PKG_VERSION=${PKG_VERSION:="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | cut -d "-" -f1)"}
+PKG_3DIGIT_VERSION=${PKG_3DIGIT_VERSION:="$(echo "${PKG_VERSION}" | cut -d "." -f1-3)"}
 TIMESTAMP=${TIMESTAMP:=$(sh -c 'date "+%Y%m%d%H%M"')}
 NIFI_ROOT=${NIFI_ROOT:="${INSTALLATION_PREFIX}/${PKG_NAME}/${PKG_NAME}-${PKG_3DIGIT_VERSION}"}
 
